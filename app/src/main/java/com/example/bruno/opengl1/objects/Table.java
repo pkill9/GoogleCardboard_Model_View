cd5f0662590c80cba74a79dba5394d9fddab166e
pkill9 <brunocalabria92@gmail.com>
2014-11-30 03:47:18 -0500

    New Files for rendering models

--- app/src/main/java/com/example/bruno/opengl1/objects/Table.java
+++ app/src/main/java/com/example/bruno/opengl1/objects/Table.java
@@ -11,8 +11,7 @@ import static com.example.bruno.opengl1.Constants.BYTES_PER_FLOAT;
 public class Table {
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int TEXTURE_COORDINATES_COMPONENT_COUNT = 2;
-    private static final int STRIDE = (POSITION_COMPONENT_COUNT
-            + TEXTURE_COORDINATES_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + TEXTURE_COORDINATES_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
     private static final float[] VERTEX_DATA = {
             // Order of coordinates: X, Y, S, T
